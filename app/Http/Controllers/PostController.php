ab49da4394101474fe047947795ae1e4e3b2c940
GitHubのユーザ名 <GitHub>
2023-08-14 15:45:33 +0900

    ADD secondsetting

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,6 +12,7 @@ class PostController extends Controller//インポートしたPostをインス�
 {
     public function index(Post $post)
     {
-        return $post->get();//$postの中身を戻り値にする。
+        return view('posts.index')->with(['posts' => $post->get()]);
+        //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入
     }
 }
